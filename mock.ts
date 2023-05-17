51b6f821d53914f8b05d01b648ba16d5451996f9
Ruslan <61955096+ruslanpascoal2@users.noreply.github.com>
2023-05-17 19:28:16 -0300

    init

---
+++ mock.ts
@@ -0,0 +1,32 @@
+export const _mock = [
+  {
+    id: 1,
+    title: 'Apartment 3rd floor',
+    desc: 'Spacious apartment located on the 3rd floor with beautiful views of the city. It features a modern kitchen, a large living room, and two bedrooms. The apartment is conveniently located near shopping centers and public transportation.',
+    image: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80'
+  },
+  {
+    id: 2,
+    title: 'Luxury Penthouse',
+    desc: 'Experience luxury living in this stunning penthouse apartment. It offers panoramic views of the city skyline, top-of-the-line amenities, and spacious rooms designed with elegance and style. The penthouse includes a private terrace, a state-of-the-art kitchen, and a master bedroom suite with a jacuzzi.',
+    image: 'https://images.unsplash.com/photo-1502672023488-70e25813eb80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2264&q=80'
+  },
+  {
+    id: 3,
+    title: 'Cozy Townhouse',
+    desc: 'This charming townhouse is located in a peaceful neighborhood and offers a cozy living environment. It features a well-equipped kitchen, a comfortable living room, and a lovely backyard garden. The townhouse is within walking distance to parks, schools, and local amenities.',
+    image: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80'
+  },
+  {
+    id: 4,
+    title: 'Modern Studio Apartment',
+    desc: 'This modern studio apartment offers a sleek and minimalist living space. It is equipped with the latest appliances, has large windows that provide ample natural light, and includes a comfortable sleeping area and a small office corner. The apartment is located in a vibrant neighborhood with trendy cafes and shops nearby.',
+    image: 'https://plus.unsplash.com/premium_photo-1683769252575-e986af039184?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80'
+  },
+  {
+    id: 5,
+    title: 'Spacious Family Home',
+    desc: 'Ideal for families, this spacious home features multiple bedrooms, a large backyard, and a cozy family room. It offers plenty of space for relaxation and entertainment. The home is located in a quiet neighborhood close to schools, parks, and shopping centers.',
+    image: 'https://images.unsplash.com/photo-1630699144867-37acec97df5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80'
+  }
+];
